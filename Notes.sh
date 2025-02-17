c3fb862f96f851b4f3735b57a4b59199bba14b03
yves40 <yves.toubhans@free.fr>
2025-02-17 12:49:52 +0100

    Comment

--- Notes.sh
+++ Notes.sh
@@ -4,10 +4,10 @@ npm install create-next-app@latest --save
 # Install a new app named 1.app-router
 # Look here https://nextjs.org/docs/app/api-reference/cli/create-next-app
 npx create-next-app@latest 1.app-router --js --tailwind --turbopack --eslint --app --src-dir src
-
+# From the top directory
 git init
-git add README.md
-git commit -m "first commit"
+git commit -m "Initial"
 git branch -M main
 git remote add origin https://github.com/yves40/NEXTJS.git
 git push -u origin main
+# Add .gitignore file in the top dir

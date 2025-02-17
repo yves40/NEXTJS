2beaa815fe4bfeac752a48f5bf170727bbdd436b
yves40 <yves.toubhans@free.fr>
2025-02-17 12:37:57 +0100

    Initial

---
+++ Notes.sh
@@ -0,0 +1,13 @@
+# Install next and the app creation tool in the top directory ( here NEXTJS )
+npm install next@latest --save
+npm install create-next-app@latest --save
+# Install a new app named 1.app-router
+# Look here https://nextjs.org/docs/app/api-reference/cli/create-next-app
+npx create-next-app@latest 1.app-router --js --tailwind --turbopack --eslint --app --src-dir src
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yves40/NEXTJS.git
+git push -u origin main

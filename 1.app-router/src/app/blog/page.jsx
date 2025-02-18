ab4fb1c8c77c2c66bf081fc1b3657bfa5875fb55
yves40 <yves.toubhans@free.fr>
2025-02-18 09:08:14 +0100

    Lesson : Céer des routes : END

---
+++ 1.app-router/src/app/blog/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function blog() {
+  return (
+    <main className=' text-4xl text-cyan-600 mt-10'>
+      <h1>Blog page</h1>
+    </main>
+  )
+}
---
+++ 1.app-router/src/app/dashboard/entreprise/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main className=' text-4xl text-cyan-600 mt-10'>
+      <h1>Enterprise page</h1>
+    </main>
+  )
+}

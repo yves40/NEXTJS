7e3a9b796873971638188f2a9c8adcb6ded06621
yves40 <yves.toubhans@free.fr>
2025-02-20 19:24:31 +0100

    Lesson : Routes dynamiques : END

--- 1.app-router/src/app/blog/[id]/page.jsx
+++ 1.app-router/src/app/blog/[id]/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link';
 
-function page() {
+async function page({params}) {
+
+  const {id} = await params;
+
+  const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const post = await resp.json();
+
   return (
     <div className=' text-4xl text-red-600 m-20'>
-      <p>Route dynamique</p>
+      <p className=' font-bold mb-4'>{post.title}</p>
+      <p className=' text-sm font-normal  mb-4'>{post.body}</p>
+      <Link href="/blog" className=' text-gray-600 text-[1.3rem] hover:underline'>&#8592; Retour aux blogs</Link>
     </div>
   )
 }

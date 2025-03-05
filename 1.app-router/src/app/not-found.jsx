fb4d229c559a3dc15dd54a450f9fecd13233a37e
yves40 <yves.toubhans@free.fr>
2025-03-05 10:23:22 +0100

    Lesson : gestion d'erreur 404

---
+++ 1.app-router/src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <main>
+      <p className=" bg-red-600 text-xl m-10 p-10 text-white">Erreur 404, damned</p>
+    </main>
+  )
+}
+
+export default NotFound

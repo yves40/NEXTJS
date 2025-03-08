8a83e0ddd826d95c3906802360f7b12c04e5c6fb
yves40 <yves.toubhans@free.fr>
2025-03-08 12:06:04 +0100

    Lesson : Server action : Part 2 : My share

--- 2.composants-et-data/src/app/form/page.jsx
+++ 2.composants-et-data/src/app/form/page.jsx
@@ -1,10 +1,34 @@
 "use client"  // Must be specified to use WEB apis on client
 
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { getPost } from "@/lib/serveraction"
 
 function page() {
 
   const [inputValue, setInputValue] = useState("")
+  const [status, setStatus] = useState('Paused')
+  const [postindex, setPostIndex] = useState(1);
+  const [postMessage, setPostMessage] = useState('');
+  const msgref = useRef();
+  
+  // Track message call
+  useEffect( () => {
+    console.log('useEffect triggered');
+  }, [postindex])
+  
+  async function handleGetArticle() {
+    setStatus('Requesting')
+    const post = await getPost(postindex);
+    setStatus(`Done : got post with ID ${post.id}`);
+    setPostIndex(postindex + 1);
+    setPostMessage(post.title);
+    // 1st method to update the message title : getElementById
+    const mshandle = document.getElementById('msgid');
+    mshandle.classList.remove('hidden');
+    // 2nd method : just use useRef() ;-)
+    msgref.current.classList.remove('hidden');
+    msgref.current.style.color = 'red';
+  }
 
   return (
     <div className=" m-10">
@@ -13,6 +37,11 @@ function page() {
           onChange={ e => setInputValue(e.target.value)}
           placeholder="Entrez du texte"/>
       <p>Tu as écrit ! {inputValue}</p>
+      <button className=" bg-green-400 border-none rounded-2xl my-3 p-2 text-white"
+       onClick={handleGetArticle}>Get Article</button>
+       <p>Status : {status} </p>
+       <p id="msgid" className="hidden">Post title : {postMessage}</p>
+       <p ref={msgref} className="hidden">Post title : {postMessage}</p>
     </div>
   )
 }

ac1d45de3aead2e72f11d06605c73a0a2f5618e0
yves40 <yves.toubhans@free.fr>
2025-03-10 18:32:17 +0100

    Tailwind & nextJS

--- 3.styles-et-optimisation/src/app/page.jsx
+++ 3.styles-et-optimisation/src/app/page.jsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main>
       <h1 className=" text-2xl">Styles et optimisation</h1>
+      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
+      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
     </main>
   );
 }

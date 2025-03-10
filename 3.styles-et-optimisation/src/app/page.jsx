d9f9d183f1a4762e145ec710d20ccccd7d82dd93
yves40 <yves.toubhans@free.fr>
2025-03-10 18:49:22 +0100

    Lesson : sass & nextJS : END

--- 3.styles-et-optimisation/src/app/page.jsx
+++ 3.styles-et-optimisation/src/app/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Card from "@/components/Card/Card";
 
 export default function Home() {
   return (
     <main>
       <h1 className=" text-2xl">Styles et optimisation</h1>
-      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
-      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
+      <Card></Card>
     </main>
   );
 }
---
+++ 3.styles-et-optimisation/src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.scss'
+
+function Card() {
+  return (
+    <div className=' p-10'>
+      <p className='card'>Card</p>
+      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
+      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
+    </div>
+  )
+}
+
+export default Card

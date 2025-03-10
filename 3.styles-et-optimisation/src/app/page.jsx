a57a8e50965ea438261a0995ece6c438d8b42386
yves40 <yves.toubhans@free.fr>
2025-03-10 19:01:33 +0100

    Lesson : Image optimization: 1

--- 3.styles-et-optimisation/src/app/page.jsx
+++ 3.styles-et-optimisation/src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Card from "@/components/Card/Card";
+import speed from '../../public/images/speedtriple.jpg'
 
 export default function Home() {
   return (
-    <main>
+    <main className=" max-w-5xl mx-auto">
       <h1 className=" text-2xl">Styles et optimisation</h1>
       <Card></Card>
+      <Image src={speed} alt="The speed"/>
     </main>
   );
 }

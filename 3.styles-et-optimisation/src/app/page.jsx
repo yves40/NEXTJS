8f28bf3280439089acb83238c27dc222dd2bb29a
yves40 <yves.toubhans@free.fr>
2025-03-11 08:07:56 +0100

    Lesson : Image optimization: END

--- 3.styles-et-optimisation/src/app/page.jsx
+++ 3.styles-et-optimisation/src/app/page.jsx
@@ -7,7 +7,11 @@ export default function Home() {
     <main className=" max-w-5xl mx-auto">
       <h1 className=" text-2xl">Styles et optimisation</h1>
       <Card></Card>
-      <Image src={speed} alt="The speed"/>
+      <Image src={speed} alt="The speed" sizes="
+      (max-width: 500px) 500px,
+      (max-width: 800px) 800px,
+      (max-width: 1080px) 1080px,
+      4096px" className=" border-none rounded-2xl"/>
     </main>
   );
 }
--- 3.styles-et-optimisation/src/components/Card/Card.jsx
+++ 3.styles-et-optimisation/src/components/Card/Card.jsx
@@ -5,7 +5,6 @@ function Card() {
     <div className=' p-10'>
       <p className='card'>Card</p>
       <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
-      <p className=" p-4 text-cyan-600 max-lg:">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam dolore veniam officia veritatis recusandae excepturi consectetur, numquam totam amet aliquid.</p>
     </div>
   )
 }

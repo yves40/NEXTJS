931459348c2e0838076c3a214dfc9afba09f3b13
yves40 <yves.toubhans@free.fr>
2025-03-11 10:20:47 +0100

    PROJECT : Page d'accueil statique  : END

--- 4.axoria-blog/src/app/page.jsx
+++ 4.axoria-blog/src/app/page.jsx
@@ -1,7 +1,63 @@
+import Link from "next/link"
+
+const fakeposts = [
+  {
+    author: 'John Doe',
+    title: '5 CSS tricks'
+  },
+  {
+    author: 'Victor Wallas',
+    title: 'How to code a Navbar'
+  },
+  {
+    author: 'Jean Dupont',
+    title: 'How to setup TS'
+  },
+  {
+    author: 'Isabelle Sodernous',
+    title: 'Launching satellites is not a small business'
+  },
+  {
+    author: 'Yves Oracleous',
+    title: 'Proprietary software cannot be as good as open source software'
+  },
+]
+
 export default async function Home() {
   return (
-    <div className=" pt-44">
-      <h1>Axoria Blog : #1</h1>
+    // u-main-container is defined in globals.css 
+    <div className="u-main-container u-padding-content-container">
+      <h1 className="t-main-title">Stay up to date with AXORIA</h1>
+      <p className="t-main-subtitle">Tech news and useful knowledge about the web best practices.
+        This is the mandatory basic knowledge you must have to pretend being a nextJS developer!
+      </p>
+      <p className=" mr-4 text-md text-zinc-900">Latest articles
+        <ul className="u-articles-grid">
+            {
+              fakeposts.map( (post, id) => (
+                <li className=" rounded-sm shadow-md hover:shadow-xl border hover:border-zinc-300">
+                  <div className=" pt-5 px-5 pb-7">
+                    <div className="flex items-baseline gap-x-4 text-xs">
+                      <time datetime={new Date().toISOString()} className=" text-gray-500 text-sm">
+                        {new Date().toLocaleDateString("en-EN", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric"
+                        })}
+                      </time>
+                      <Link href={`/categories/author/${post.author}`} className=" ml-auto text-base text-gray-70
+                       hover:text-gray-600 whitespace-nowrap truncate">
+                        {post.author}
+                      </Link>
+                    </div>
+                    <Link href={`/article/${post.title}`} className=" inline-block mt-6 font-semibold text-zinc-800
+                     hover:text-zinc-600">{post.title}</Link>
+                  </div>
+                </li>
+              ))
+            }
+        </ul>
+      </p>
     </div>
   )
 }
--- 4.axoria-blog/src/components/Footer.jsx
+++ 4.axoria-blog/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 function Footer() {
   return (
     <footer className=" text-center bg-white p-4 border-t border-t-zinc-300">
-      <Link href="#">Axoria &copy; : Mar 11 2025 1.00</Link>
+      <Link href="#">Axoria &copy; : Mar 11 2025 1.01</Link>
     </footer>
   )
 }

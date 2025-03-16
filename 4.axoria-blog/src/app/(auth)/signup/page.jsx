0b0b2415976f9f98dd9f3672a9d7d6935a4b4b51
yves40 <yves.toubhans@free.fr>
2025-03-16 19:08:37 +0100

    PROJECT : 1st user creation in mongo !!

--- 4.axoria-blog/src/app/(auth)/signup/page.jsx
+++ 4.axoria-blog/src/app/(auth)/signup/page.jsx
@@ -2,21 +2,50 @@
 
 import React, { useRef } from 'react'
 import Link from 'next/link';
+import { register } from '@/lib/serverActions/session/sessionServerAction';
+import { useRouter } from "next/navigation";
 
 export default function page() {
 
-    const pseudo = useRef('pseudo');
-    const email = useRef('email');
-    const password = useRef('password');
-    const confpassword = useRef('confpassword');
+    const userName = useRef('');
+    const email = useRef('');
+    const password = useRef('');
+    const confpassword = useRef('');
+    const submitButton = useRef('');
+    const serverInfo = useRef('');
+    const router = useRouter();
+    
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        console.log('Now registering');
         const formData = new FormData(e.target);
         const formdataObj = Object.fromEntries(formData);
         console.log(JSON.stringify(formdataObj));
-        
+        serverInfo.current.textContent = ""
+        submitButton.current.disabled = true;   // No multiple server request when one is running
+        try {
+            const result = await register(new FormData(e.target));            
+            if(result.success) {
+            // if(true) {
+                submitButton.current.textContent = 'User saved ✅';
+                let countdown = 3;
+                serverInfo.current.textContent = `Redirecting ${countdown}...`;
+                const interval = setInterval(() => {
+                  countdown -= 1;
+                  serverInfo.current.textContent = `Redirecting ${countdown}...`;
+                  if(countdown === 0) {
+                    clearInterval(interval);
+                    router.push(`/signin`); // In case of success route to login page
+                  }
+                }, 1000);        
+            }
+        }
+        catch(error) {
+            submitButton.current.textContent = 'Submit';
+            submitButton.current.disabled = false;
+            console.log(error);            
+            serverInfo.current.textContent = "An error occured";
+        }
     }
 
     return (
@@ -26,17 +55,19 @@ export default function page() {
             <div className='w-7/8 md:w-1/2 border rounded shadow-md background-slate-900 text-left mx-auto
                     m-4 p-4'>
                 <form onSubmit={handleSubmit}>
-                    <label className='f-label' htmlFor="pseudo">Pseudo</label>
-                    <input className='f-auth-input' type="text" name="pseudo" id="pseudo" ref={pseudo} placeholder='Choose a pseudo'/>
+                    <label className='f-label' htmlFor="userName">Name</label>
+                    <input className='f-auth-input' type="text" name="userName" id="userName" ref={userName} placeholder='Name or Pseudo'/>
                     <label className='f-label' htmlFor="email">E-mail</label>
                     <input className='f-auth-input' type="text" name="email" id="email" ref={email} placeholder='Your contact email'/>
                     <label className='f-label' htmlFor="password">Password</label>
                     <input className='f-auth-input' type="password" name="password" id="password" ref={password} placeholder='Your password'/>
                     <label className='f-label' htmlFor="confpassword">Confirm password</label>
                     <input className='f-auth-input' type="password" name="confpassword" id="confpassword" ref={confpassword} placeholder='Confirrm password'/>
-                    <button className='w-full bg-blue-500 hover:bg-blue-800 text-white mt-6 rounded-lg border p-4'>Submit</button>
+                    <button className='w-full bg-blue-500 hover:bg-blue-800 text-white mt-6 rounded-lg border p-4'
+                     ref={submitButton}>Submit</button>
                 </form>
             </div>
+            <p ref={serverInfo} className=' text-center my-4'></p>
             <Link href={"/signin"} className=' mt-6 text-blue-500 underline'>Already have an account ? Log In</Link>
         </div>
     )
---
+++ 4.axoria-blog/src/lib/serverActions/session/sessionServerAction.js
@@ -0,0 +1,50 @@
+"use server"
+
+import { User } from "@/lib/models/user";
+import { connectToDB } from "@/lib/utils/db/connectToDB";
+import slugify from "slugify";
+import bcrypt from 'bcryptjs';
+
+export async function register(formData) {
+    const { userName, email, password, confpassword } = Object.fromEntries(formData);
+    const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;        
+
+    try {
+        if(userName < 3) {
+            throw new Error("Pseudo too short, at least 3 characters");
+        }
+        const validEmail = emailregex.test(email);
+        if(!validEmail) {
+            throw new Error("Invalid email");
+        }
+        if(password < 6) {
+            throw new Error("password must have at least 6 characters");
+        }
+        if(password !== confpassword) {
+            throw new Error("password and confirm password must be the same");
+        }
+    
+        connectToDB();
+        const user = await User.findOne({userName});
+        if(user) {
+            throw new Error("User already exists, sory !");
+        }
+        const normalizedUserName = slugify(userName, {lower:true,strict:true});
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+    
+        const newUser = new User({
+            userName,
+            normalizedUserName,
+            email,
+            password: hashedPassword
+        })
+        await newUser.save();
+        console.log(`saved user to DB : ${JSON.stringify(newUser)}`);
+        return { success: true };
+    }
+    catch(error) {
+        throw new Error(error.message || 'An error occured while registering the user')
+    }
+
+}

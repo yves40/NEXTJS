159892e7bfbe02b3e4e9214edb12f95a6626f09a
yves40 <yves.toubhans@free.fr>
2025-03-17 19:12:17 +0100

    Add an import Tag otherwise get an error as a non registered schema

--- 4.axoria-blog/src/lib/serverMethods/blog/postMethods.js
+++ 4.axoria-blog/src/lib/serverMethods/blog/postMethods.js
@@ -1,15 +1,19 @@
 import { connectToDB } from "@/lib/utils/db/connectToDB";
 import { Post } from "@/lib/models/post";
+import { Tag } from "@/lib/models/tag";
+
+const modulename = "POST #";
 
 export async function getPost(slug) {
     try 
     {
-        await connectToDB();
-        const post = await Post.findOne({slug}).populate({
+        // await connectToDB();
+        console.log(`${modulename} Getting details for post ${slug}`);
+        const post = await Post.findOne({slug});
+        return post.populate({
             path: "tags",
             select: "name slug"
         });
-        return post;
     }
     catch(error){
         console.log(`Error fetching the post ${slug}`, error);

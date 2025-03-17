4e4d1fd76a896fdbe95a3f05d748de96740f9f28
yves40 <yves.toubhans@free.fr>
2025-03-17 10:01:49 +0100

    PROJECT : Create the login page

--- 4.axoria-blog/src/app/(auth)/signin/page.jsx
+++ 4.axoria-blog/src/app/(auth)/signin/page.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client"
+
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
+import Link from 'next/link';
 
 export default function page() {
+  const userName = useRef('');
+  const password = useRef('');
+  const submitButton = useRef('');
+  const serverInfo = useRef('');
+  const router = useRouter();
+
+  async function handleSubmit(e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const formdataObj = Object.fromEntries(formData);
+      console.log(JSON.stringify(formdataObj));
+  }
+      
+      
   return (
-    <div className='u-main-container u-padding-content-container'>
-        <h1>Sign In</h1>
-    </div>
-  )
+    <div className='u-main-container u-padding-content-container 
+      flex flex-col mx-auto text-center background-slate-500'>
+      <h1 className='mb-10 text-2xl'>Sign In</h1>
+      <div className='w-7/8 md:w-1/2 border rounded shadow-md background-slate-900 text-left mx-auto
+          m-4 p-4'>
+      <form onSubmit={handleSubmit}>
+          <label className='f-label' htmlFor="userName">Name</label>
+          <input className='f-auth-input' type="text" name="userName" id="userName" ref={userName} placeholder='Name or Pseudo'/>
+          <label className='f-label' htmlFor="password">Password</label>
+          <input className='f-auth-input' type="password" name="password" id="password" ref={password} placeholder='Your password'/>
+          <button className='w-full bg-blue-500 hover:bg-blue-800 text-white mt-6 rounded-lg border p-4'
+          ref={submitButton}>Submit</button>
+      </form>
+      </div>
+    <p ref={serverInfo} className=' text-center my-4'>Status</p>
+    <Link href={"/signup"} className=' mt-6 text-blue-500 underline'>Need an account ? Register</Link>
+  </div>
+)
 }

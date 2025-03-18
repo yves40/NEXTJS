8bb0d55f410ae2135115a1b004300155890de657
yves40 <yves.toubhans@free.fr>
2025-03-18 17:06:58 +0100

    PROJECT : Manage mose clicks on navbar dropdown

---
+++ 4.axoria-blog/src/app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <main>
+        <h1 className='u-main-container u-padding-content-container text-2xl text-blue-400'>Dashboard</h1>
+    </main>
+  )
+}

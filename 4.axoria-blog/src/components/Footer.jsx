a14c8255f82871119b5871a9dc76d0363c287fe9
yves40 <yves.toubhans@free.fr>
2025-03-23 10:41:46 +0100

    Version. Back to RECO

--- 4.axoria-blog/src/components/Footer.jsx
+++ 4.axoria-blog/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className=" text-center bg-white p-4 border-t border-t-zinc-300">
       <Link href="#">
-        Axoria &copy; : Mar 20 2025 : 1.12
+        Axoria &copy; : Mar 23 2025 : 1.13
       </Link>
     </footer>
   )

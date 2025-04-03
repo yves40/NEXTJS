5af66c111296c04529d9a62bccab88e865e1aec3
yves40 <yves.toubhans@free.fr>
2025-04-03 18:12:25 +0200

    PROJECT : WIP image display in article

--- 4.axoria-blog/src/app/article/[slug]/page.jsx
+++ 4.axoria-blog/src/app/article/[slug]/page.jsx
@@ -1,5 +1,6 @@
 
 import { getPost } from "@/lib/serverMethods/blog/postMethods"
+import Image from "next/image";
 import Link from "next/link"
 import "./article-styles.css"
 import 'prism-themes/themes/prism-vsc-dark-plus.css'
@@ -8,12 +9,17 @@ export default async function page({params}) {
 
     const {slug} = await params;
     const post = await getPost(slug);
+    const imagePath = `/blogimages/${post.imageFile}`;
 
     return (
     <div>
         <main className="u-main-container u-padding-content-container">
             <h1 className=" text-4xl mb-3">{post.title}</h1>
             <h2 className=" mx-2  mb-2 font-semibold italic text-2xl">By..., {post.author}</h2>
+            <Image src={imagePath} alt="The speed"
+                width={500}
+                height={250}
+                className=" border-none rounded-2xl  object-cover"/>
             <p className=" mb-6">
                 {post.tags.map( tag => (
                     <Link key={tag.slug} className=" mr-4 underline" href={`categories/tag/${tag.slug}`}>#{tag.name}</Link>

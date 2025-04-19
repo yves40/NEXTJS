3eeea15dbad2cda25019fe5542fede04aac37e12
yves40 <yves.toubhans@free.fr>
2025-04-19 11:30:58 +0200

    Change some UI attributes

--- 4.axoria-blog/src/app/not-found.jsx
+++ 4.axoria-blog/src/app/not-found.jsx
@@ -2,10 +2,11 @@ import Link from "next/link"
 
 export default function notfound() {
   return (
-    <div className=" pt-44 text-center">
-        <h1 className=" text-4xl mb-4">404 - Not found</h1>
-        <p className=" mb-2">Could not find requested resource</p>
-        <Link href="/" className="underline">Return Home</Link>
+    <div className=" pt-44 text-center bg-gray-700">
+        <h1 className=" text-white text-4xl mb-4">404 - Not found</h1>
+        <p className=" mb-2 text-red-500">Could not find requested resource</p>
+        <p className=" my-4 text-red-400">Please contact the site admin on 06.88.33.55.66</p>
+        <Link href="/" className="underline text-red-400 mb-10">Return Home</Link>
     </div>
   )
 }

a0364b669cb72d6b0c14cb10e0007b331ea22b3e
yves40 <yves.toubhans@free.fr>
2025-04-19 11:31:09 +0200

    Version

--- 4.axoria-blog/src/components/Footer.jsx
+++ 4.axoria-blog/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className=" text-center bg-white p-4 border-t border-t-zinc-300">
       <Link href="#">
-        Axoria &copy; : Apr 15 2025 : 1.18
+        Axoria &copy; : Apr 19 2025 : 1.19
       </Link>
     </footer>
   )
